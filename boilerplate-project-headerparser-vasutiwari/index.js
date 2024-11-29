30431c65f2ad17bb6eeac6f466c2b993885685b5
Vasu Tiwari <Vasu Tiwari@DESKTOP-94E3FQN>
2024-11-28 21:42:38 -0500

    Project 2 complete

---
+++ boilerplate-project-headerparser-vasutiwari/index.js
@@ -0,0 +1,46 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function (req, res) {
+  var ipaddress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  if (ipaddress.startsWith("::ffff:")) {
+    ipaddress = ipaddress.replace("::ffff:", ""); // Remove the IPv6 prefix
+  }
+
+  res.json({ 
+    ipaddress: ipaddress.split(",")[0].trim(),
+    language : language,
+    software : software
+   });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
